5d762110be0d1d9818a11f227c55ff56123b0332
darwankaramrut26 <darwankaramrut26@gmail.com>
2026-04-15 17:09:49 +0530

    all test added

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -2,7 +2,10 @@ class HomePage {
     constructor(page) {
         this.page = page;
         this.productList = page.locator('.inventory_list');
-        this.profileMenu = page.locator('#react-burger-menu-btn');
+        this.cartIcon = page.locator('//button[@id="add-to-cart-sauce-labs-backpack"]');
+        this.cartBadge = page.locator('//div[@id="shopping_cart_container"]//span[@class="shopping_cart_badge"]');
+        this.checkoutButton = page.locator('//button[@id="checkout"]');
+        // this.profileMenu = page.locator('#react-burger-menu-btn');
         this.logoutButton = page.locator('#logout_sidebar_link');
     }
 
@@ -16,6 +19,9 @@ class HomePage {
 
     async logout() {
         await this.profileMenu.click();
+        await this.cartIcon.click();
+        await this.cartBadge.click();
+        await this.checkoutButton.click();
         await this.logoutButton.click();
     }
 }
